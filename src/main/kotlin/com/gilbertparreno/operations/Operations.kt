062db1ff30dc6e0b96ff70e5a920de3832b680d9
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-02-17 18:11:14 +0800

    Initial commit

---
+++ src/main/kotlin/com/gilbertparreno/operations/Operations.kt
@@ -0,0 +1,8 @@
+package com.gilbertparreno.operations
+
+object Operations {
+    fun add(a: Int, b: Int) = a + b
+    fun subtract(a: Int, b: Int) = a - b
+    fun multiply(a: Int, b: Int) = a * b
+    fun divide(a: Int, b: Int) = a / b
+}
